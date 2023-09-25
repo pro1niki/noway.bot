b8f203dc07dae9b345dc41b63f5a4bb25935f61e
“Nico <nb98real@gmail.com>
2023-09-25 18:16:45 -0300

    full
    bot de ticket discord

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    token: "",
+    prefix: "!",
+    intents:  [
+        'GUILDS',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGES',
+        'GUILD_INVITES',
+        'GUILD_VOICE_STATES',
+        'GUILD_MEMBERS',
+        'GUILD_PRESENCES'
+    ],
+    footer: "Cidade™ Pixel ©️ Todos os direitos reservados.",
+    semperm: ":x: **‣** Você não possui a permissão necessaria para executar este comando.",
+    onwerBot: "",
+	cargos: {
+		membro: "",
+		notificacao: {
+			anuncios: "",
+			atualizacoes: "",
+			parcerias: ""
+		},
+		staffs: {
+			suporte: "",
+			moderador: "",
+			staff: "",
+            admin: "",
+            developer: "",
+            manager: "",
+			fundador: ""
+		}
+	}
+}
